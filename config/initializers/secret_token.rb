c5f65701ddd11cb2cec2ab74386bc0075e5742cf
Thomas Ciccarelli <tom@gianttitan.com>
2013-01-18 22:24:06 -0500

    Bare Site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '151d7f9b2f5c18b815680ef2f6ea7c7145935c2c133698f144ac9b184ee8353bc4a5c73cfdf80f631c8a84160f3830d04731137ae7b594cc2bbb29467550a681'
